35395317ae76a5022e9cde9dd87fdaad4f1fb85f
freevedo <dyfreg75@gmail.com>
2020-12-05 23:24:04 +0100

    add topbar for number,email

---
+++ resources/views/inc/topbar.blade.php
@@ -0,0 +1,19 @@
+<section id="topbar" class="d-none d-lg-block">
+    <div class="container clearfix">
+      <div class="contact-info float-left">
+<!--Ajout des donnees contact a partir de la bd-->
+        <i class="icofont-envelope"></i><a href="mailto:contact@example.com">contact@example.com</a>
+        <i class="icofont-phone"></i> +1 5589 55488 55
+
+      </div>
+      <div class="social-links float-right">
+            <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="" class="instagram"><i class="icofont-youtube"></i></a>
+        <a href="#" class="skype"><i class="icofont-skype"></i></a>
+        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></a>
+    
+        
+      </div>
+    </div>
+  </section>
