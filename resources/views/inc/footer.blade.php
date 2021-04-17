919af64f630f44c1aa4ccdf8acae83f3fa1556f7
freevedo <dyfreg75@gmail.com>
2021-04-17 19:01:38 +0200

    17.04

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -6,8 +6,8 @@
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-3 col-md-6 footer-info">
-            <h3>SosBloodbf</h3>
+          <div class="col-lg-4 col-md-6 footer-info">
+            <h2 class="titel-logo">Fasoblood</h2>
             <p>
               <strong>Phone:</strong> +226 71 73 69 56<br>
               <strong>Email:</strong>sosbloodbf@gmail.com<br>
@@ -18,16 +18,39 @@
             </div>
           </div>
 
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Liens utiles</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right "></i> <a class="linkmain" href="{{ url('/') }}">Home</a></li>
-              <li><i class="bx bx-chevron-right "></i> <a class="linkmain" href="{{ url('/#about') }}">About us</a></li>
-              <li><i class="bx bx-chevron-right "></i> <a class="linkmain" href="{{ url('/#departments') }}">Services</a></li>
-            </ul>
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Sitemap</h4>
+            <div class="container">
+              <ul>
+                <li><i class="bx bx-chevron-right "></i> <a class="linkmain" href="{{ url('/') }}">Home</a></li>
+                <li><i class="bx bx-chevron-right "></i> <a class="linkmain" href="{{ url('/#about') }}">About us</a></li>
+                <li><i class="bx bx-chevron-right "></i> <a class="linkmain" href="{{ url('/#departments') }}">Services</a></li>
+                <li><i class="bx bx-chevron-right "></i> <a class="linkmain" href="{{ url('/alert') }}">Alertes</a></li>
+                <li><i class="bx bx-chevron-right "></i> <a class="linkmain" href="{{url('/alert/create') }}">Envoyer une alerte</a></li>
+                <li><i class="bx bx-chevron-right "></i> <a class="linkmain" href="{{ route('register') }}">S'enregistrer</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 footer-form" >
+            <h4>Contactez nous</h4>
+              <div class="form-group col-lg-6">
+                <label for=""></label>
+                <input type="text" name="name" id="name" placeholder="Nom complet">
+              </div>
+              <div class="form-group col-lg-6">
+                <label for=""></label>
+                <input type="text" name="email" id="email" placeholder="Adresse email">
+              </div>
+              <div class="form-group col-lg-6">
+                <label for=""></label>
+                <textarea name="message" id="message" cols="30" rows="5"></textarea>
+              </div>
+              {{-- <input type="text" name="" id="">
+              <input type="text" name="" id="">
+              <input type="  " style="width: 100%"> --}}
           </div>
         </div>
       </div>
-      <div class="copyright"> <h5>Copyright 2021 © Tous droits reserves. By SosBloodbf</h5></div>
+      <div class="copyright"> <p> 2021 © Tous droits reserves. By Fasoblood</p></div>
  
 </footer>
